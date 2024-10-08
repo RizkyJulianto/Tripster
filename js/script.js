dfc26b23741d33c255233aab25515276e29580e8
Juliantomaga <juliantorizky01@gmail.com>
2024-10-08 21:51:39 +0700

    make responsive in home section

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Show Navigation Bar in Mobile
+const navBar = document.getElementById("navigation-list");
+function showMenu() {
+    navBar.style.right = '0';
+}
+
+function closeMenu() {
+    navBar.style.right = '-210px';
+}
