0166ba659f67762680c573e55737e76ee85b9e2b
Juliantomaga <juliantorizky01@gmail.com>
2024-10-11 15:40:14 +0700

    change background color navbar

--- js/script.js
+++ js/script.js
@@ -12,21 +12,9 @@ function closeMenu() {
 window.addEventListener('scroll' ,function() {
     const ScrollY = window.pageYOffset;
     const navBar = document.getElementById('navBar');
-    const logo = document.querySelector('.logo a');
-    const navList = document.querySelectorAll('.navigation-list ul li a')
     if(ScrollY > 5) {
-        navBar.style.backdropFilter = 'blur(5px)';
-        navBar.style.boxShadow = '0 4px 6px 0 rgba(0,0,0,0.7)';
-        logo.style.color = '#ef4444';
-        navList.forEach(item => {
-            item.style.color = "#041126";
-        })
+        navBar.style.backgroundColor = '#041126';
     } else {
-        navBar.style.backdropFilter = 'blur(0)';
-        navBar.style.boxShadow = 'none';
-        logo.style.color = '#ffffff';
-        navList.forEach(item => {
-            item.style.color = "#ffffffb3";
-        })
+        navBar.style.backgroundColor = 'transparent';
     }
 })
